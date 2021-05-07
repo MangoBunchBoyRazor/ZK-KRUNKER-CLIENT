995c2b07d1399e75abbdb62b6fc698522c6c9387
BluZed <atulyajain28122007@gmail.com>
2021-05-08 00:13:37 +0530

    auto-update

---
+++ src/index.js
@@ -0,0 +1,202 @@
+//modules
+require("v8-compile-cache"); //For better startup
+const { autoUpdater } = require('electron-updater');
+const isDev = require('electron-is-dev');
+const path = require("path");
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const shortcuts = require("electron-localshortcut");
+const Store = require("electron-store");
+Menu.setApplicationMenu(null);
+const config = new Store();
+//const DiscordRPC = require("discord-rpc");
+const fs = require("fs");
+const { dir } = require("console");
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+//Performance improvements
+app.commandLine.appendSwitch("force_high_performance_gpu"); //Use better gpu
+app.commandLine.appendSwitch("force-high-performance-gpu"); //Use better gpu-try2
+app.commandLine.appendSwitch("disable-frame-rate-limit"); //Uncap fps
+app.commandLine.appendSwitch("disable-gpu-vsync"); //Uncap fps 2
+app.commandLine.appendSwitch("in-process-gpu"); //Performance improve
+app.commandLine.appendSwitch("ignore-gpu-blacklist"); //Performance improve
+
+let win;
+
+const createWindow = () => {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1120,
+    height: 635,
+    frame: true,
+	titleBarStyle: 'hidden',
+    show: false,
+    icon: __dirname + "/media/icon/icon.ico",
+    webPreferences: {
+      nodeIntergation: true,
+      preload: path.join(__dirname, "preload.js"),
+      webSecurity: false,
+    },
+  });
+
+ //Splash-
+  splash = new BrowserWindow({width: 810, height: 610, transparent: true, frame: false, alwaysOnTop: true});
+  splash.loadFile(path.join(__dirname, "splash.html"));
+  //-
+
+  win.loadFile(path.join(__dirname, "index.html"));
+
+  if (config.get("enablePointerLockOptions", false)) {
+    app.commandLine.appendSwitch("enable-pointer-lock-options");
+  }
+  let contents = win.webContents;
+  shortcuts.register(win, "Escape", () =>
+    contents.executeJavaScript("document.exitPointerLock()", true)
+  );
+
+  win.once("ready-to-show", () => {
+    win.show();
+	autoUpdater.checkForUpdatesAndNotify();
+    splash.destroy();
+  });
+};
+
+app.allowRendererProcessReuse = true;
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+//Auto Updates
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
+
+//RPC CODE
+const clientId = "826389993122562048";  
+
+DiscordRPC.register(clientId);
+
+const rpc = new DiscordRPC.Client({ transport: "ipc" });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc || !win) {
+    setTimeout(() => {
+      setActivity();
+    }, 15000);
+
+    return;
+  }
+
+  rpc.setActivity({
+    details: `Shooting peeps`,
+    state: "Playing on ZK Client",
+    startTimestamp,
+    largeImageKey: "icon_512x512",
+    largeImageText: "ZK Client",
+  });
+}
+
+rpc.on("ready", () => {
+  setActivity();
+});
+
+rpc.login({ clientId }).catch(console.error);
+
+//Resource Swapper
+const urls = [];
+const css = [];
+let newPath;
+const getCSSFiles = (pa) => {
+  try {
+    fs.readdirSync(pa, { withFileTypes: true }).forEach((dir) => {
+      css.push(`${pa}/${dir.name}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+const recursiveSwap = (dpath) => {
+  try {
+    fs.readdirSync(dpath, { withFileTypes: true }).forEach((dir) => {
+      if (dir.isDirectory()) {
+        if (dir.name == "css") getCSSFiles(`${dpath}/${dir.name}`);
+        recursiveSwap(`${dpath}/${dir.name}`);
+      } else {
+        newPath = `${dpath}/${dir.name}`;
+        const cPath = newPath.replace(
+          `${app.getPath("documents")}\\ZK-SWAP`,
+          ""
+        );
+
+        if (cPath.indexOf("models") != -1 || cPath.indexOf("textures") != -1) {
+          urls.push(
+            `*://assets.krunker.io${cPath}*`,
+            `*://assets.krunker.io${cPath}?*`
+          );
+        } else
+          urls.push(
+            `*://krunker.io${cPath}*`,
+            `*://krunker.io${cPath}?*`,
+            `*://comp.krunker.io${cPath}*`,
+            `*://comp.krunker.io${cPath}?*`
+          );
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//Looking for resource swapper directory in documents
+const dirPath = path.join(app.getPath("documents"), "ZK-SWAP");
+
+if (fs.existsSync(dirPath)) {
+  recursiveSwap(dirPath);
+} else {
+  fs.mkdirSync(dirPath);
+  recursiveSwap(dirPath);
+}
+
+ipcMain.on("swapFiles", () => {
+  win.webContents.send("css-urls", { urls: css });
+  if (urls[0]) {
+    win.webContents.session.webRequest.onBeforeRequest(
+      { urls: urls },
+      (details, callback) => {
+        callback({
+          redirectURL:
+            "file:///" + path.join(dirPath, new URL(details.url).pathname),
+        });
+      }
+    );
+  }
+});
+
+
---
+++ src/preload.js
@@ -0,0 +1,45 @@
+const { ipcRenderer } = require("electron");
+let cssurls;
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (document.location.href.startsWith("https://krunker.io/")) {
+    ipcRenderer.send("swapFiles");
+  }
+});
+
+window.prompt = importSettings = () => {
+  const tempHTML = `<div class="setHed">Import Settings</div>
+    <div class="settName" id="importSettings_div" style="display:block">Settings -<input type="url" placeholder="Paste Settings Here" name="url" class="inputGrey2" id="settingString"></div>
+    <a class="+" id="importBtn">Import</a>`;
+  menuWindow.innerHTML = tempHTML;
+  importBtn.addEventListener("click", () => {
+    parseSettings(settingString.value);
+  });
+  parseSettings = (string) => {
+    if (string) {
+      try {
+        const json = JSON.parse(string);
+        for (const setting in json) {
+          setSetting(setting, json[setting]);
+          showWindow(1);
+        }
+      } catch (err) {
+        console.error(err);
+        alert("Error importing settings.");
+      }
+    }
+  };
+};
+
+ipcRenderer.on("css-urls", (e, d) => {
+  cssurls = d.urls;
+  cssurls.forEach((url) => {
+    const cssEl = document.createElement("link");
+    cssEl.href = url;
+    cssEl.rel = "stylesheet";
+    document.head.appendChild(cssEl);
+  });
+});
+
+//Client popup workaround from idkr
+window.OffCliV = true;
